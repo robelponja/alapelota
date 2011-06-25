8bafa4c746c6325ce14f2e846d9f485c685b261a
federico martinez <fmartinez@plata.(none)>
2011-06-24 21:10:08 -0300

    agregando archivos de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alapelota::Application.config.secret_token = '46f2bd5a08d6fb1e366134ee943f980a97a5f357d83abe8841780161602daaa9b953238a7d1b66181761a7d53312d7024a58bac48c980037a21d2feb4202b284'
